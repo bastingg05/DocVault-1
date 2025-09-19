b69cb66d93c088f799518975adbf0f377b5c6a6b
Bastin George <bastingg05@gmail.com>
2025-09-20 05:06:26 +0530

    deploy: trigger Vercel deployment with CORS fixes for mobile access

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 // DocVault - Updated for new deployment
+// Triggering Vercel deployment with CORS fixes
 function App() { return (<div style={{padding:20}}>DocuVault Frontend</div>) }
 
 export default App
